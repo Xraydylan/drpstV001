76c40ca72fa42f3a68c0bff1b7c8af2051ecdf65
Xraydylan <37886333+Xraydylan@users.noreply.github.com>
2018-04-02 14:58:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 
 
 def path():
+    print(os.path.abspath(__file__))
     print(os.path.dirname(os.path.abspath(__file__)))
+    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 
